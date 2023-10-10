1d24886fc75e43d494523d9f32f055db3df7e865
Luciano Martin Gamberale <lgamberale@fi.uba.ar>
2023-10-10 17:47:55 -0300

    Testing par_iter

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,40 +1,46 @@
-use std::{
-    collections::HashMap,
-    error::Error,
-    fs::{read_dir, File},
-    io::{BufRead, BufReader},
-    time::Instant,
-};
+use std::collections::HashMap;
+use std::fs::{read_dir, File};
+use std::io::{BufRead, BufReader};
+use std::{env, thread};
 
-fn main() -> Result<(), Box<dyn Error>> {
+use rayon::prelude::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
+
+fn main() {
+    counting_words();
+}
+
+fn counting_words() {
     let start = Instant::now();
-
-    let result = read_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/dataset"))
+    let result = read_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/data"))
         .unwrap()
         .map(|d| d.unwrap().path())
+        .collect::<Vec<PathBuf>>()
+        .par_iter()
         .flat_map(|path| {
             let file = File::open(path);
             let reader = BufReader::new(file.unwrap());
-            reader.lines()
+            reader.lines().par_bridge()
         })
         .map(|l| {
-            if let Ok(line) = l {
-                let words = line.split(' ');
-                // thread::sleep(Duration::from_millis(100));
-                let mut counts = HashMap::new();
-                words.for_each(|w| *counts.entry(w.to_string()).or_insert(0) += 1);
-                counts
-            }
+            let line = l.unwrap();
+            let words = line.split(' ');
+            thread::sleep(Duration::from_millis(100));
+            let mut counts = HashMap::new();
+            words.for_each(|w| *counts.entry(w.to_string()).or_insert(0) += 1);
+            counts
         })
-        .fold(HashMap::new(), |mut acc, words| {
-            words
-                .iter()
-                .for_each(|(k, v)| *acc.entry(k.clone()).or_insert(0) += v);
-            acc
-        });
+        .reduce(
+            || HashMap::new(),
+            |mut acc, words| {
+                words
+                    .iter()
+                    .for_each(|(k, v)| *acc.entry(k.clone()).or_insert(0) += v);
+                acc
+            },
+        );
+    println!("{:?}", start.elapsed());
 
     println!("{:?}", result);
-
-    println!("{:?}", start.elapsed());
-    Ok(())
 }
